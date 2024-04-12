30edad8041ba86587bf49092b7ba835f3c0aaf46
userjack6880 <userjack6880@gmail.com>
2024-04-11 22:44:07 -0500

    doing stuff

--- fluid.cc
+++ fluid.cc
@@ -144,10 +144,10 @@ void copyPeriodic(float *p, float *u, float *v, float *w,
 // Before summing up fluxes, zero out the residual term
 void zeroResidual(float *presid, float *uresid, float *vresid, float *wresid,
                   int ni, int nj, int nk , int kstart, int iskip, int jskip) {
+  const int kskip=1;
         #pragma omp parallel
       {
         #pragma omp for
-  const int kskip=1;
   for(int i=-1; i<ni+1; ++i) {
     for(int j=-1; j<nj+1; ++j) {
       int offset = kstart+i*iskip+j*jskip;
