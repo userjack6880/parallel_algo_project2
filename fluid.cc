13e3847bdfd5dfe989a32bf389115faa988fac0f
userjack6880 <userjack6880@gmail.com>
2024-04-11 23:39:51 -0500

    doing stuff

--- fluid.cc
+++ fluid.cc
@@ -135,10 +135,10 @@ void zeroResidual(float *presid, float *uresid, float *vresid, float *wresid,
   const int kskip=1;
         #pragma omp parallel
       {
+        #pragma omp for
   for(int i=-1; i<ni+1; ++i) {
     for(int j=-1; j<nj+1; ++j) {
       int offset = kstart+i*iskip+j*jskip;
-              #pragma omp for
       for(int k=-1;k<nk+1;++k) {
         const int indx = k+offset;
         presid[indx] = 0;
